00be646145d5661a209ba03eb6e10bb3e82c4b3a
Jusousa7 <juleite236@gmail.com>
2024-11-14 11:43:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao (
+    'Programaçaõ',
+    'O que é Python',
+    'O Python é uma linguagem de programação'
+)
+
+
+criaCartao (
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao (
+    'Programaçao',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao (
+    'Lingua inglesa',
+    'Como se diz oi em inglês?',
+    'Oi em inglês é HI (RAI)'
+)
